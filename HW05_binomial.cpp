167633dc260d789a735ed9c1aafc449f072d80c0
kchearin <147133498+kchearin@users.noreply.github.com>
2023-10-06 16:31:19 +0900

    Add files via upload

--- HW05_binomial.cpp
+++ HW05_binomial.cpp
@@ -6,17 +6,17 @@ float factorial(int n) { if (n == 0 | n == 1) return 1; else return n * factoria
 int main() {
 	float p, * biex, * bitheory;
 	int n, k, N = 100000, count;  // N number of experiment
-	n = 20;  p = 0.5;
+	n = 20;  p = 1.0 / 6.0;
 	biex = new float[n + 1];
 	bitheory = new float[n + 1];
 	for (k = 0; k <= n; k++) biex[k] = 0;
 	for (int nn = 0; nn < N; nn++) {
 		count = 0;
-		for (int j = 0; j < 10; j++) if ((rand() / (float)RAND_MAX) < p) count++;
+		for (int j = 0; j < n; j++) if ((rand() / (float)RAND_MAX) < p) count++;
 		biex[count]++;
 	} // nn
 	for (int k = 0; k <= n; k++) biex[k] /= (float)N; // count --> probability
-	ofstream xxx("binomial_p0.5.txt");
+	ofstream xxx("binomial__1.txt");
 	for (int k = 0; k <= n; k++) {
 		bitheory[k] = factorial(n) / factorial(k) / factorial(n - k) * pow(p, k) * pow((1 - p), n - k);
 		xxx << k << " " << bitheory[k] << " " << biex[k] << endl;
